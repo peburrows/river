1dc58d108ec0c226d5ab490d3c49408abf3f8295
Phil Burrows <github@philburrows.com>
2016-10-04 23:29:55 -0500

    :new: version 0.0.2 --> 0.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule River.Mixfile do
 
   def project do
     [app: :river,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.3",
      description: description,
      package: package,
